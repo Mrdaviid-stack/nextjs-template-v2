027b7cdedc46dde15f0bda597646f2af49ae5098
Mark David <mrkdvdbgyn@gmail.com>
2023-04-09 12:34:51 +0800

    add accordion UI

---
+++ src/pages/components/Accordion.tsx
@@ -0,0 +1,88 @@
+'use client'
+import { useState } from "react";
+import { FiPlus, FiMinus } from "react-icons/fi";
+interface AccordionProps {
+  title: string
+  children: React.ReactNode
+}
+
+const Accordion: React.FC<AccordionProps> = ({
+  title,
+  children,
+}) => {
+
+  const [expanded, setExpanded] = useState(false)
+  const toggleExpanded = () => setExpanded((current) => !current)
+
+  return (
+    <>
+      <div
+        onClick={toggleExpanded}
+        className="
+          flex 
+          group 
+          cursor-pointer 
+          mx-auto 
+          h-16 
+          justify-between  
+          items-center 
+          p-2 
+          rounded-md 
+          border
+        ">
+        <div 
+          className="
+            flex group 
+            cursor-pointer
+          ">
+          <div 
+            className="
+              text-pink-600 
+              font-semibold 
+              pl-10 
+            ">
+            {title}
+          </div>
+        </div>
+        <div 
+          className="
+            flex 
+            items-center 
+            justify-center 
+            pr-10
+          ">
+          {expanded ? (
+            <FiMinus 
+              className="
+                w-6 h-6 
+                text-pink-600" 
+              />
+          ) : (
+            <FiPlus className="w-6 h-6 text-pink-600" />
+          )}
+        </div>
+      </div>
+
+
+      <div 
+        className={` 
+          border-blue-300  
+          text-pink-500 
+          pl-10  
+          p-4 
+          mb-2 
+          font-semibold 
+          h-auto  
+          rounded-md 
+          border-l-2
+          ${expanded ? 'block' : 'hidden'}
+          `}
+        >
+        {children}
+      </div>
+
+    </>
+  )
+}
+
+export default Accordion
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,41 @@
+import React, { useState } from "react";
+import Accordion from "./components/Accordion";
 
 export default function Home() {
+  const [index, setIndex] = useState(false);
+
+  const data = [
+    {
+      id: 0,
+      question: "What is Next.Js ?",
+      answer: "The React Framework for Production",
+    },
+    {
+      id: 1,
+      question: "What is Tailwindcss ?",
+      answer:
+        "A utility-first CSS framework packed with classes like flex, pt-4, text-center and rotate-90 that can be composed to build any design, directly in your markup.",
+    },
+    {
+      id: 2,
+      question: "What is art ?",
+      answer:
+        " a visual object or experience consciously created through an expression of skill or imagination.",
+    },
+  ];
   return (
+    <>
     <div className="flex justify-center items-center text-rose-500 text-2xl">NextJS Template V2</div>
+      {data.map((data) => {
+        return (
+          <Accordion
+            key={data.id}
+            title={data.question}
+            children={data.answer}
+          />
+        );
+      })}
+
+    </>
   )
 }
